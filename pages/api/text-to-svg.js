1e64f2c32083846ae2a5ceb4c4446ac3c48ecaf4
Dani Fornells <dfornells@kavehome.com>
2022-03-01 23:45:00 +0100

    Adjust TMP folder

--- pages/api/text-to-svg.js
+++ pages/api/text-to-svg.js
@@ -1,3 +1,4 @@
+import { tmpdir } from "os"
 import TextToSVG from "text-to-svg/index.js"
 import { optimize } from "svgo/lib/svgo.js"
 import got from "got"
@@ -12,8 +13,8 @@ export default async function handler(req, res) {
     }
     const fontDetails = await got(`https://google-webfonts-helper.herokuapp.com/api/fonts/${font}`).json()
     const fontURL = fontDetails.variants.find(v => v.id === style).ttf
-    await download(fontURL, 'tmp', {filename: `${font}.ttf`})
-    const textToSVG = TextToSVG.loadSync(`tmp/${font}.ttf`)
+    await download(fontURL, tmpdir(), {filename: `${font}.ttf`})
+    const textToSVG = TextToSVG.loadSync(`/tmp/${font}.ttf`)
     const options = {x: 0, y: 0, fontSize: 72, anchor: 'top'}
     const svg = optimize(textToSVG.getSVG(text, options)).data
     // Cache headers and final response
